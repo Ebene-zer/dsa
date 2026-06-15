72d845ca0b625c8e7fb21e6cd63791a59eb3c9f8
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-06-15 16:10:34 +0000

    implement stack operations with java collection

--- structures/stackStructure.java
+++ structures/stackStructure.java
@@ -1,6 +1,21 @@
 package structures;
+import java.util.Stack;
+
 public class stackStructure {
     public static void main(String[] args){
-        System.out.println("Hello");
+        Stack<Integer> numValue = new Stack<>();
+
+        numValue.push(10);
+        numValue.push(5);
+
+        System.out.println("The top most number is: " + numValue.peek());
+        System.out.println("Found and removed " + numValue.pop());
+        System.out.println("The top most number is: " + numValue.peek());
+        System.out.println("Found and removed " + numValue.pop());
+        System.out.println("The stack is empty: " + numValue.isEmpty());
+
+        numValue.push(15);
+         System.out.println("The stack is empty: " + numValue.isEmpty());
+
     }
 }
