34acd73ea9f83aedc3aa4b27f1fa28bb8d6a3a7d
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-06-25 20:02:04 +0000

    add size check

--- structures/stackStructure.java
+++ structures/stackStructure.java
@@ -8,16 +8,20 @@ public class stackStructure {
         numValue.push(10);
         numValue.push(5);
 
+        System.out.println("The size of the stack is: " + numValue.size());
         System.out.println("The top most number is: " + numValue.peek());
         System.out.println("Found and removed " + numValue.pop());
+        System.out.println("The size of the stack is: " + numValue.size());
         System.out.println("The top most number is: " + numValue.peek());
         System.out.println("Found and removed " + numValue.pop());
+        System.out.println("The size of the stack is: " + numValue.size());
         System.out.println("The stack is empty: " + numValue.isEmpty());
 
         numValue.push(15);
-         System.out.println("The stack is empty: " + numValue.isEmpty());
+        System.out.println("The size of the stack is: " + numValue.size());
+        System.out.println("The stack is empty: " + numValue.isEmpty());
+        
 
     }
 }
 
-//Next: Stack implementaion using an array
